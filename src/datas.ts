4bfaa24e93921265f51339e2c831b50f60e1c289
Mustafa Emin Bosnalı <mustafaeminbosnali@hotmail.com>
2021-11-15 19:31:17 +0300

    datas.ts; New datas added.

---
+++ src/datas.ts
@@ -0,0 +1,98 @@
+export const alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+export interface IUser {
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  email: string | null;
+}
+
+export const users = [
+  {
+    firstName: "Abdul",
+    lastName: "Aziz",
+    phoneNumber: "+90 552 625 58 84",
+    email: "abdulaziz@hotmail.com",
+  },
+  {
+    firstName: "Ali",
+    lastName: "Kamışçı",
+    phoneNumber: "+90 567 586 34 63",
+    email: null,
+  },
+  {
+    firstName: "Ahmet",
+    lastName: "Ulu",
+    phoneNumber: "+90 593 386 25 63",
+    email: "ahmetulu@hotmail.com",
+  },
+  {
+    firstName: "Bamsı",
+    lastName: "Bilgin",
+    phoneNumber: "+90 532 385 78 33",
+    email: "bamsibilgin@hotmail.com",
+  },
+  {
+    firstName: "Beyza",
+    lastName: "Altın",
+    phoneNumber: "+90 535 756 85 84",
+    email: "beyzaaltin@hotmail.com",
+  },
+  {
+    firstName: "Canan",
+    lastName: "Haluk",
+    phoneNumber: "+90 582 651 65 46",
+    email: "cananhaluk@hotmail.com",
+  },
+  {
+    firstName: "Deniz",
+    lastName: "Bilgin",
+    phoneNumber: "+90 542 384 34 63",
+    email: null,
+  },
+  {
+    firstName: "Derya",
+    lastName: "Derin",
+    phoneNumber: "+90 524 438 35 78",
+    email: "deryaderin@hotmail.com",
+  },
+  {
+    firstName: "Efe",
+    lastName: "Soylu",
+    phoneNumber: "+90 559 325 35 43",
+    email: null,
+  },
+  {
+    firstName: "Zekariya",
+    lastName: "Öz",
+    phoneNumber: "+90 546 454 46 54",
+    email: "zekariyaoz@hotmail.com",
+  },
+];
