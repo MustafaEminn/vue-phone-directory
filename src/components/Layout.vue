e488bdbb89b4bfbb1f40520291cffc8889f5f8e6
Mustafa Emin Bosnalı <mustafaeminbosnali@hotmail.com>
2021-11-15 19:31:07 +0300

    Layout.vue; Props added. Max width and centering added to container. header style added.

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,20 +1,39 @@
 <template>
   <div class="layoutContainer">
     <div class="layoutContainer_header">
-      <slot name="header" />
+      {{ props.header }}
     </div>
     <slot />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { defineProps, defineExpose, defineComponent } from "vue";
+
+defineComponent({
+  name: "Layout",
+});
+
+interface IProps {
+  header: string;
+}
+const props = defineProps<IProps>();
+
+defineExpose({
+  props,
+});
+</script>
 
 <style lang="scss">
 .layoutContainer {
   display: flex;
   flex-direction: column;
+  max-width: 1396px;
+  margin: 0 auto;
 
   & .layoutContainer_header {
+    border: 2px solid gray;
+    font-size: var(--h2);
   }
 }
 </style>
