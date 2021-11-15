da5a8eb90c3fe54754a1f9e83fec4ed58e0a41c5
Mustafa Emin Bosnalı <mustafaeminbosnali@hotmail.com>
2021-11-16 01:01:07 +0300

    HeaderBar.vue; Added and contunies.

---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="headerBarContainer">
+    <div class="headerBarContainer_backButton">
+      <router-link to="/">
+        <Button type="success" :disabled="false">
+          <template #icon>
+            <LeftArrow class="headerBarContainer_backButtonIcon"
+          /></template>
+        </Button>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.headerBarContainer {
+  display: flex;
+
+  & .headerBarContainer_backButton {
+    & .headerBarContainer_backButtonIcon {
+    }
+  }
+}
+</style>
+
+<script setup lang="ts">
+import LeftArrow from "./icons/LeftArrow.vue";
+import Button from "./Button.vue";
+</script>
