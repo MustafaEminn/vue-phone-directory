b2cfa69c210315caeae40230ad267961099fc25a
Mustafa Emin Bosnalı <mustafaeminbosnali@hotmail.com>
2021-11-17 00:44:02 +0300

    HeaderBar.vue; Search section added.

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,13 +1,27 @@
 <template>
   <div class="headerBarContainer">
     <div class="headerBarContainer_backButton">
-      <router-link to="/">
-        <Button type="success" :disabled="false">
+      <router-link v-show="showBackButton" to="/">
+        <Button :circle-border="true" :border="false" :disabled="false">
           <template #icon>
-            <LeftArrow class="headerBarContainer_backButtonIcon"
+            <LeftArrowIcon class="headerBarContainer_backButtonIcon"
           /></template>
         </Button>
       </router-link>
+    </div>
+    <div
+      class="headerBarContainer_search"
+      :class="
+        classNames({
+          headerBarContainer_searchLeftPadding: !showBackButton,
+        })
+      "
+    >
+      <TextInput placeholder="Search..." :full-width="true" :rounded="true">
+        <template #icon>
+          <SearchIcon class="headerBarContainer_searchIcon"
+        /></template>
+      </TextInput>
     </div>
   </div>
 </template>
@@ -15,15 +29,46 @@
 <style lang="scss">
 .headerBarContainer {
   display: flex;
+  align-items: center;
+  width: 100%;
+  padding-top: 0.6rem;
 
   & .headerBarContainer_backButton {
     & .headerBarContainer_backButtonIcon {
+      width: 1.5rem;
+    }
+  }
+
+  & .headerBarContainer_search {
+    width: 100%;
+    padding: 0 1rem 0 0;
+
+    &.headerBarContainer_searchLeftPadding {
+      padding-left: 1rem;
+    }
+    & .headerBarContainer_searchIcon {
+      width: 1.2rem;
     }
   }
 }
 </style>
 
 <script setup lang="ts">
-import LeftArrow from "./icons/LeftArrow.vue";
+import { defineProps, defineExpose } from "vue";
+import classNames from "classnames";
+import LeftArrowIcon from "./icons/LeftArrowIcon.vue";
+import SearchIcon from "./icons/SearchIcon.vue";
 import Button from "./Button.vue";
+import TextInput from "./TextInput.vue";
+
+interface IHeaderBarProps {
+  showBackButton?: boolean;
+}
+
+const props = defineProps<IHeaderBarProps>();
+
+defineExpose({
+  props,
+  classNames,
+});
 </script>
