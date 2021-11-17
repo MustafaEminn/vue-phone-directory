74f2defac77d5d6bcb64a843c9bbef27f434bbd6
Mustafa Emin Bosnalı <mustafaeminbosnali@hotmail.com>
2021-11-17 20:00:50 +0300

    App.vue; LocalStorage check added.

--- src/App.vue
+++ src/App.vue
@@ -5,3 +5,11 @@
 <style lang="scss">
 @import "./styles/base/index.scss";
 </style>
+
+<script setup lang="ts">
+import { users } from "./datas";
+
+if (!localStorage.contacts) {
+  localStorage.contacts = JSON.stringify(users);
+}
+</script>
