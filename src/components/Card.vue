6c0a2632a8649e3412c53303f233a3f2f39a0c5c
Mustafa Emin Bosnalı <mustafaeminbosnali@hotmail.com>
2021-11-17 11:54:17 +0300

    Card.vue; Added.

---
+++ src/components/Card.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    class="cardContainer"
+    :class="
+      classNames({
+        cardContainerFullWidth: fullWidth,
+      })
+    "
+  >
+    <slot />
+  </div>
+</template>
+
+<style lang="scss">
+.cardContainer {
+  width: min-content;
+  box-shadow: var(--box-shadow-1);
+  border: 2px solid var(--lightgray-3);
+  padding: 0.75rem;
+  border-radius: 4px;
+
+  &.cardContainerFullWidth {
+    width: 100%;
+  }
+}
+</style>
+
+<script lang="ts" setup>
+import { defineProps } from "vue";
+import classNames from "classnames";
+
+interface ICardProps {
+  fullWidth?: boolean;
+}
+
+defineProps<ICardProps>();
+</script>
